97ed46a7538fc6a07d73ece92c4737b06145999c
bipin kumar <bipinbce@users.noreply.github.com>
2020-12-14 00:07:01 +0530

    create curd operation in wcf

---
+++ EmployeeService/EmployeeService.svc.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace EmployeeService
+{  
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.  
+    public class EmployeeService : IEmployeeService   
+    {
+
+        public static string ConnectionString = "Data Source=DESKTOP-8A8TA50;Initial Catalog=EmployeeDb;User ID=sa;Password=Mssqlserver@8083;";
+        public string GetData(int value)  
+        {  
+            return string.Format("You entered: {0}", value);  
+        }  
+  
+        //C- Add Employee Record  
+        public string AddEmployyeeRecord(Employee emp)   
+        {  
+            string result = "";  
+            try  
+            {  
+  
+                SqlConnection con = new SqlConnection(ConnectionString);  
+                SqlCommand cmd = new SqlCommand();                  
+  
+                string Query = @"INSERT INTO tblEmployee (EmpID,Name,Email,Phone,Gender)  
+                                               Values(@EmpID,@Name,@Email,@Phone,@Gender)";  
+  
+                cmd = new SqlCommand(Query, con);  
+                cmd.Parameters.AddWithValue("@EmpID", emp.EmpID);  
+                cmd.Parameters.AddWithValue("@Name", emp.Name);  
+                cmd.Parameters.AddWithValue("@Email", emp.Email);  
+                cmd.Parameters.AddWithValue("@Phone", emp.Phone);  
+                cmd.Parameters.AddWithValue("@Gender", emp.Gender);  
+                con.Open();  
+                cmd.ExecuteNonQuery();  
+                con.Close();  
+                result = "Record Added Successfully !";  
+            }  
+            catch (FaultException fex)  
+            {  
+                result = "Error";  
+            }  
+  
+             return result;               
+        }  
+  
+        //Retrieve Data  
+        //Retrive Record  
+        public DataSet GetEmployeeRecords()  
+        {  
+            DataSet ds = new DataSet();  
+            try  
+            {  
+                SqlConnection con = new SqlConnection(ConnectionString);  
+                string Query = "SELECT * FROM tblEmployee";  
+  
+                SqlDataAdapter sda = new SqlDataAdapter(Query, con);  
+                sda.Fill(ds);  
+            }  
+            catch (FaultException fex)  
+            {  
+                throw new FaultException<string>("Error: "+fex);  
+            }  
+              
+            return ds;  
+        }  
+  
+        //Delete Record  
+        public string DeleteRecords(Employee emp)  
+        {  
+            string result = "";  
+            SqlConnection con = new SqlConnection(ConnectionString);  
+            SqlCommand cmd = new SqlCommand();  
+            string Query = "DELETE FROM tblEmployee Where EmpID=@EmpID";   
+            cmd = new SqlCommand(Query, con);  
+            cmd.Parameters.AddWithValue("@EmpID", emp.EmpID);  
+            con.Open();  
+            cmd.ExecuteNonQuery();  
+            con.Close();  
+            result = "Record Deleted Successfully!";  
+            return result;  
+        }  
+  
+        //Search Employee Record  
+        public DataSet SearchEmployeeRecord(Employee emp)  
+        {  
+            DataSet ds = new DataSet();  
+            try  
+            {  
+                SqlConnection con = new SqlConnection(ConnectionString);  
+                string Query = "SELECT * FROM tblEmployee WHERE EmpID=@EmpID";  
+  
+                SqlDataAdapter sda = new SqlDataAdapter(Query, con);  
+                sda.SelectCommand.Parameters.AddWithValue("@EmpID", emp.EmpID);  
+                sda.Fill(ds);  
+            }  
+            catch (FaultException fex)  
+            {  
+                throw new FaultException<string>("Error:  " + fex);  
+            }  
+            return ds;          
+        }  
+  
+        //UPDATE RECORDS  
+        //Update by Phone Roll   
+        public string UpdateEmployeeContact(Employee emp)   
+        {  
+            string result = "";  
+            SqlConnection con = new SqlConnection(ConnectionString);  
+            SqlCommand cmd = new SqlCommand();  
+  
+            string Query = "UPDATE tblEmployee SET Email=@Email,Phone=@Phone WHERE EmpID=@EmpID";  
+  
+            cmd = new SqlCommand(Query, con);  
+            cmd.Parameters.AddWithValue("@EmpID", emp.EmpID);  
+            cmd.Parameters.AddWithValue("@Email", emp.Email);  
+            cmd.Parameters.AddWithValue("@Phone", emp.Phone);  
+            con.Open();  
+            cmd.ExecuteNonQuery();  
+            result = "Record Updated Successfully !";  
+            con.Close();  
+  
+            return result;  
+        }  
+          
+          
+    }  
+}  
